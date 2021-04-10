379d9c042133b7f60aef88a639bd9d617aff8480
BearingBall <74752877+BearingBall@users.noreply.github.com>
2021-04-10 13:20:43 +0700

    bugz

--- realsense_photo.py
+++ realsense_photo.py
@@ -7,6 +7,7 @@ import pickle
 def takePhoto(fileName):
     pipeline = rs.pipeline()
     config = rs.config()
+    os.makedirs(fileName)
     
     pipeline_wrapper = rs.pipeline_wrapper(pipeline)
     pipeline_profile = config.resolve(pipeline_wrapper)
@@ -60,14 +61,14 @@ def takePhoto(fileName):
                 cv2.destroyAllWindows()
                 break
             if key == 32:
-                file = open(fileName+"_"+str(i)+".configs", "wb")
+                file = open(fileName+"/"+str(i)+".configs", "wb")
                 configs = [bg_removed.shape[0], bg_removed.shape[1] , bg_removed.shape[2], depth_image.shape[0], depth_image.shape[1 ], depth_scale]
                 pickle.dump(configs, file)
                 file.close
-                file = open(fileName+"_"+str(i)+".color", "wb")
+                file = open(fileName+"/"+str(i)+".color", "wb")
                 pickle.dump(bg_removed, file)
                 file.close()
-                file = open(fileName+"_"+str(i)+".depth", "wb")
+                file = open(fileName+"/"+str(i)+".depth", "wb")
                 pickle.dump(depth_image, file)
                 file.close()
                 print("taked "+str(i))
@@ -80,19 +81,19 @@ def takePhoto(fileName):
 def showPhoto(fileName):
     i=0
     isRefresh = True
-    point = np.zeros(4)
     currentLabel = 0
-    labels = ["floor" , "roof", "wall", "door", "obstacle"]
+    labels = ["floor" , "roof", "wall", "door", "obstacle", "stairUp", "stairDown"]
+    figure = np.zeros(9)
     while True:
         if isRefresh:
             isRefresh = False
-            file = open(fileName+"_"+str(i)+".configs", "rb")
+            file = open(fileName+"/"+str(i)+".configs", "rb")
             configs = pickle.load(file)
             file.close()
-            file = open(fileName+"_"+str(i)+".color", "rb")
+            file = open(fileName+"/"+str(i)+".color", "rb")
             bg_removed = pickle.load(file)
             file.close()
-            file = open(fileName+"_"+str(i)+".depth", "rb")
+            file = open(fileName+"/"+str(i)+".depth", "rb")
             depth_image = pickle.load(file)
             file.close()
             
@@ -109,30 +110,37 @@ def showPhoto(fileName):
             images = images.astype(np.uint8)
         
         def draw_circle(event,x,y,flags,param):
-            if event == cv2.EVENT_LBUTTONDOWN:
-                point[0] = x
-                point[1] = y
+            #if event == cv2.EVENT_LBUTTONDOWN:
             if event == cv2.EVENT_LBUTTONUP:
-                point[2] = x
-                point[3] = y
-                tmp = np.zeros(5)
-                tmp[0] = point[0]
-                tmp[1] = point[1]
-                tmp[2] = point[2]
-                tmp[3] = point[3]
-                tmp[4] = currentLabel
-                param.append(tmp)
-                
+                param[int(param[8])*2] = x
+                param[int(param[8])*2+1] = y
+                param[8]+=1
+        
+        
+        if (int(figure[8]) == 4):
+                    figure[8] = currentLabel
+                    configs.append(figure)
+                    figure = np.zeros(9)
+                    
+        
         cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)
-        cv2.setMouseCallback('Align Example',draw_circle, configs)
+        cv2.setMouseCallback('Align Example',draw_circle, figure)
         
         images = np.hstack((bg_removed, depth_colormap))
         images = images.astype(np.uint8)
+        for j in range(0, int(figure[8])):
+            cv2.circle(images, (int(figure[j*2]),int(figure[j*2+1])), 4, (0,0,255), 2)
         for j in range(6, len(configs)):
-            cv2.rectangle(images,(int(configs[j][0]),int(configs[j][1])),(int(configs[j][2]),int(configs[j][3])), (255,0,0),1)
-            cv2.putText(images, labels[int(configs[j][4])] , (int(min(configs[j][0],configs[j][2] )),int(min(configs[j][1],configs[j][3]))+30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255),2)
+            #cv2.rectangle(images,(int(configs[j][0]),int(configs[j][1])),(int(configs[j][2]),int(configs[j][3])), (255,0,0),1)
+            cv2.line(images, (int(configs[j][0]), int(configs[j][1])), (int(configs[j][2]), int(configs[j][3])), (255,0,0), 1)
+            cv2.line(images, (int(configs[j][2]), int(configs[j][3])), (int(configs[j][4]), int(configs[j][5])), (255,0,0), 1)
+            cv2.line(images, (int(configs[j][4]), int(configs[j][5])), (int(configs[j][6]), int(configs[j][7])), (255,0,0), 1)
+            cv2.line(images, (int(configs[j][6]), int(configs[j][7])), (int(configs[j][0]), int(configs[j][1])), (255,0,0), 1)
+            
+            cv2.putText(images, labels[int(configs[j][8])] , (int((configs[j][0]+configs[j][2]+configs[j][4]+configs[j][6])/4-50),int((configs[j][1]+configs[j][3]+configs[j][5]+configs[j][7])/4)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255),2)
         for j in range(len(labels)):
             cv2.putText(images, labels[j] , (650, 40+j*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0 if j == currentLabel else 255, 0, 255),2)
+            
         cv2.putText(images, fileName+"_"+str(i) , (900, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255),2)
         cv2.imshow('Align Example',  images)
         
@@ -142,23 +150,79 @@ def showPhoto(fileName):
         
         if key & 0xFF == ord('q') or key == 27:
             cv2.destroyAllWindows()
-            file = open(fileName+"_"+str(i)+".configs", "wb")
+            file = open(fileName+"/"+str(i)+".configs", "wb")
             pickle.dump(configs, file)
             file.close()
             break
-        if (key == 46)and(os.path.isfile(fileName+"_"+str(i+1)+".configs")):
-            file = open(fileName+"_"+str(i)+".configs", "wb")
+        if (key == 46)and(os.path.isfile(fileName+"/"+str(i+1)+".configs")):
+            file = open(fileName+"/"+str(i)+".configs", "wb")
             pickle.dump(configs, file)
             file.close()
             i+=1
             isRefresh = True
-        if (key == 44)and(os.path.isfile(fileName+"_"+str(i-1)+".configs")):
-            file = open(fileName+"_"+str(i)+".configs", "wb")
+        if (key == 44)and(os.path.isfile(fileName+"/"+str(i-1)+".configs")):
+            file = open(fileName+"/"+str(i)+".configs", "wb")
             pickle.dump(configs, file)
             file.close()
             i-=1
             isRefresh = True
-        if (key < 54 and key > 48):
+        if (key < 56 and key > 48):
             currentLabel = key-49
         if (key == 122 and len(configs) >6):
             configs.pop()
+           
+
+
+def getData(fileName):
+    i=5
+    currentLabel = 0
+    figure = np.zeros(9)
+    while True:
+        file = open(fileName+"/"+str(i)+".configs", "rb")
+        configs = pickle.load(file)
+        file.close()
+        file = open(fileName+"/"+str(i)+".color", "rb")
+        bg_removed = pickle.load(file)
+        file.close()
+        file = open(fileName+"/"+str(i)+".depth", "rb")
+        depth_image = pickle.load(file)
+        file.close()
+        
+        depth_scale = configs[5]
+        clipping_distance_in_meters = 1 #1 meter
+        clipping_distance = clipping_distance_in_meters / depth_scale
+
+        bg_removed = bg_removed.reshape((int(configs[0]),int(configs[1]),int(configs[2])))
+        depth_image = depth_image.reshape((int(configs[3]),int(configs[4]),1))
+        label_image = np.zeros(int(configs[3])*int(configs[4]))
+        label_image = label_image.reshape((int(configs[3]),int(configs[4]),1))
+        
+        depth_image_3d = np.dstack((depth_image,depth_image,depth_image))
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        
+        label_colormap = cv2.applyColorMap(cv2.convertScaleAbs(label_image, alpha=0.03), cv2.COLORMAP_JET)
+        
+        for j in range(6, len(configs)):
+            #cv2.rectangle(images,(int(configs[j][0]),int(configs[j][1])),(int(configs[j][2]),int(configs[j][3])), (255,0,0),1)
+            pts = np.array([[int(configs[j][0]),int(configs[j][1])],[int(configs[j][2]),int(configs[j][3])],[int(configs[j][4]),int(configs[j][5])],[int(configs[j][6]),int(configs[j][7])]], np.int32)
+            pts = pts.reshape((-1,1,2))
+            cv2.fillPoly(label_colormap,[pts],(int(configs[j][8])*40,255-int(configs[j][8])*40,255))
+            cv2.polylines(label_colormap,[pts],True, (int(configs[j][8])*40,255-int(configs[j][8])*40,255),20)
+        cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)
+        
+        
+        
+        images = np.hstack((bg_removed, depth_colormap, label_colormap))
+        images = images.astype(np.uint8)  
+        
+
+        cv2.putText(images, fileName+"_"+str(i) , (900, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255),2)
+        cv2.imshow('Align Example',  images)
+        
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q') or key == 27:
+            cv2.destroyAllWindows()
+            file = open(fileName+"/"+str(i)+".configs", "wb")
+            pickle.dump(configs, file)
+            file.close()
+            break
